edc60d2fc42f81f0b4045a06a32ab8eb0a38c1f5
vanessA <vanesaputrianazwa@gmail.com>
2026-04-23 16:16:03 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="login-box">
+        <h1>vaksinisasi</h1>
+
+        <form action="koneksi.php" method="POST">
+
+            <label>Username</label>
+            <input type="text" name="username">
+
+            <label>Password</label>
+            <input type="password" name="password">
+
+            <button type="submit">Login</button>
+        </form>
+</body>
+</html>
